54bccde70536da6cb5ab116cf85a959a422706f5
Aleš Fabiánek <fabianek.ales@gmail.com>
2023-11-27 13:50:54 +0100

    Added getSlug for channel

--- src/types/Channel.ts
+++ src/types/Channel.ts
@@ -3,6 +3,7 @@ import { RequestContext } from "../models/RequestContext";
 
 export interface Channel {
   getName(): string;
+  getSlug(): string;
   getActions(): string[];
   serve(requestContext: RequestContext): Promise<Response>;
   getDependencyContainer(): DependencyContainer;
