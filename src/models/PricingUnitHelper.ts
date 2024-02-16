cb0d8385ec9271186d842996031bf4b0c2839a9a
Michal Panský <pansky.michal@gmail.com>
2024-02-16 13:04:44 +0100

    format

--- src/models/PricingUnitHelper.ts
+++ src/models/PricingUnitHelper.ts
@@ -1,20 +1,23 @@
-import { PricingUnit } from "@octocloud/types";
+import { PricingUnit } from '@octocloud/types';
 
 export abstract class PricingUnitHelper {
-    public static filterUnits = (units: PricingUnit[]): PricingUnit[] => {
-        const unitIds = new Set<string>();
+  public static filterUnits = (units: PricingUnit[]): PricingUnit[] => {
+    const unitIds = new Set<string>();
 
-        units.forEach((unit) => {
-            unitIds.add(unit.unitId);
-        });
+    units.forEach((unit) => {
+      unitIds.add(unit.unitId);
+    });
 
-        const uniqueUnits = Array.from(unitIds).map((unitId) => {
-            const unit = units.find((unit) => unit.unitId === unitId);
-            if (unit) {
-                return unit;
-            }
-        }).filter(Boolean) as PricingUnit[];
+    const uniqueUnits = Array.from(unitIds)
+      .map((unitId) => {
+        const unit = units.find((unit) => unit.unitId === unitId);
+        if (unit) {
+          return unit;
+        }
+        return null;
+      })
+      .filter(Boolean) as PricingUnit[];
 
-        return uniqueUnits;
-    };
+    return uniqueUnits;
+  };
 }
