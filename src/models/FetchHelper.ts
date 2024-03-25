931588a5f33cf70ebe485dafd079d13cbb2988f4
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-03-25 15:05:17 +0100

    Added FetchHelper

---
+++ src/models/FetchHelper.ts
@@ -0,0 +1,28 @@
+export class FetchHelper {
+  public async fetch(request: Request, currentRetryAttempt = 0, maxRetryAttempts = 3): Promise<Response> {
+    if (currentRetryAttempt > 0) {
+      await this.delay(Math.pow(3, currentRetryAttempt + 1) * 1000);
+    }
+
+    try {
+      const res = await fetch(request);
+      const status = res.status;
+
+      if ((status < 200 || status >= 400) && currentRetryAttempt < maxRetryAttempts) {
+        return await this.fetch(request, currentRetryAttempt + 1, maxRetryAttempts);
+      }
+
+      return res;
+    } catch (error: unknown) {
+      if (currentRetryAttempt < maxRetryAttempts) {
+        return await this.fetch(request, currentRetryAttempt + 1, maxRetryAttempts);
+      }
+
+      throw error;
+    }
+  }
+
+  private async delay(ms: number): Promise<unknown> {
+    return await new Promise((resolve) => setTimeout(resolve, ms));
+  }
+}
