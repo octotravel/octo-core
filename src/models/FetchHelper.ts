5992e1fbd31805e6da3f051e9b21d5fb53cdcedd
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-03-25 15:39:34 +0100

    added init

--- src/models/FetchHelper.ts
+++ src/models/FetchHelper.ts
@@ -1,21 +1,26 @@
 export class FetchHelper {
-  public static async fetch(request: Request, currentRetryAttempt = 0, maxRetryAttempts = 3): Promise<Response> {
+  public static async fetch(
+    input: RequestInfo,
+    init?: RequestInit,
+    currentRetryAttempt = 0,
+    maxRetryAttempts = 3,
+  ): Promise<Response> {
     if (currentRetryAttempt > 0) {
       await this.delay((currentRetryAttempt + 1) * 1000);
     }
 
     try {
-      const res = await fetch(request);
+      const res = await fetch(input, init);
       const status = res.status;
 
       if ((status < 200 || status >= 400) && currentRetryAttempt < maxRetryAttempts) {
-        return await this.fetch(request, currentRetryAttempt + 1, maxRetryAttempts);
+        return await this.fetch(input, init, currentRetryAttempt + 1, maxRetryAttempts);
       }
 
       return res;
     } catch (error: unknown) {
       if (currentRetryAttempt < maxRetryAttempts) {
-        return await this.fetch(request, currentRetryAttempt + 1, maxRetryAttempts);
+        return await this.fetch(input, init, currentRetryAttempt + 1, maxRetryAttempts);
       }
 
       throw error;
