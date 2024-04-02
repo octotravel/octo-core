887567c4427c2bd547b8c45094fdaff375a364e3
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-04-02 13:46:44 +0200

    Updated with backendParams, added tests

--- src/util/__tests__/fetchRetry.test.ts
+++ src/util/__tests__/fetchRetry.test.ts
@@ -1,5 +1,6 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { fetchRetry } from '../fetchRetry';
+import { RequestContext } from '../../models/RequestContext';
 
 describe('API', () => {
   beforeEach(() => {
@@ -9,67 +10,141 @@ describe('API', () => {
   });
 
   describe('fetch', () => {
-    it('should succeed at first try with successful response', async () => {
-      global.fetch = vi.fn().mockReturnValueOnce(Promise.resolve(new Response('', { status: 200 })));
-      const response = await fetchRetry('https://octo.ventrata.com');
-      expect(response.status).toBe(200);
+    describe('without backend params', () => {
+      it('should succeed at first try with successful response', async () => {
+        global.fetch = vi.fn().mockReturnValueOnce(Promise.resolve(new Response('', { status: 200 })));
+        const response = await fetchRetry('https://octo.ventrata.com');
+        expect(response.status).toBe(200);
+      });
+
+      it('should succeed at first try with successful response', async () => {
+        global.fetch = vi.fn().mockReturnValueOnce(Promise.resolve(new Response(null, { status: 204 })));
+        const response = await fetchRetry('https://octo.ventrata.com');
+        expect(response.status).toBe(204);
+      });
+
+      it('should succeed at first try with redirection message', async () => {
+        global.fetch = vi.fn().mockReturnValue(Promise.resolve(new Response(null, { status: 300 })));
+        const response = await fetchRetry('https://octo.ventrata.com');
+        expect(response.status).toBe(300);
+      });
+
+      it('should succeed at first try with redirection message', async () => {
+        global.fetch = vi.fn().mockReturnValue(Promise.resolve(new Response(null, { status: 303 })));
+        const response = await fetchRetry('https://octo.ventrata.com');
+        expect(response.status).toBe(303);
+      });
+
+      it('should succeed at second retry with successful response', async () => {
+        global.fetch = vi
+          .fn()
+          .mockReturnValueOnce(Promise.resolve(new Response('503 Service Unavailable', { status: 503 })))
+          .mockReturnValueOnce(Promise.resolve(new Response('{}', { status: 200 })));
+        const response = await fetchRetry('https://octo.ventrata.com');
+        expect(response.status).toBe(200);
+      });
+
+      it('should succeed at third retry with successful response', async () => {
+        global.fetch = vi
+          .fn()
+          .mockReturnValueOnce(Promise.resolve(new Response('503 Service Unavailable', { status: 503 })))
+          .mockReturnValueOnce(Promise.resolve(new Response('502 Bad Gateway', { status: 502 })))
+          .mockReturnValue(Promise.resolve(new Response('{}', { status: 200 })));
+        const response = await fetchRetry('https://octo.ventrata.com');
+        expect(response.status).toBe(200);
+      });
+
+      it('should fail after three retries with client error response', async () => {
+        global.fetch = vi
+          .fn()
+          .mockReturnValueOnce(Promise.resolve(new Response('403 Forbidden', { status: 403 })))
+          .mockReturnValueOnce(Promise.resolve(new Response('401 Unauthorized', { status: 401 })))
+          .mockReturnValue(Promise.resolve(new Response('400 Bad Request', { status: 400 })));
+        const response = await fetchRetry('https://octo.ventrata.com');
+        expect(response.status).toBe(400);
+      });
+
+      it('should fail after three retries with server error response', async () => {
+        global.fetch = vi
+          .fn()
+          .mockReturnValueOnce(Promise.resolve(new Response('503 Service Unavailable', { status: 503 })))
+          .mockReturnValueOnce(Promise.resolve(new Response('502 Bad Gateway', { status: 502 })))
+          .mockReturnValue(Promise.resolve(new Response('500 Internal Server Error', { status: 500 })));
+        const response = await fetchRetry('https://octo.ventrata.com');
+        expect(response.status).toBe(500);
+      });
     });
 
-    it('should succeed at first try with successful response', async () => {
-      global.fetch = vi.fn().mockReturnValueOnce(Promise.resolve(new Response(null, { status: 204 })));
-      const response = await fetchRetry('https://octo.ventrata.com');
-      expect(response.status).toBe(204);
-    });
+    describe('with backend params', () => {
+      let requestContext: RequestContext;
 
-    it('should succeed at first try with redirection message', async () => {
-      global.fetch = vi.fn().mockReturnValue(Promise.resolve(new Response(null, { status: 300 })));
-      const response = await fetchRetry('https://octo.ventrata.com');
-      expect(response.status).toBe(300);
-    });
+      beforeEach(() => {
+        requestContext = new RequestContext({
+          request: new Request('https://octo.ventrata.com', {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }),
+        });
 
-    it('should succeed at first try with redirection message', async () => {
-      global.fetch = vi.fn().mockReturnValue(Promise.resolve(new Response(null, { status: 303 })));
-      const response = await fetchRetry('https://octo.ventrata.com');
-      expect(response.status).toBe(303);
-    });
+        requestContext.setConnection({
+          id: '89d08dfb-6bff-4ad6-b01c-1b2f643fddce',
+          supplierId: '56e214cb-cf04-4741-8851-92603d4bda3d',
+          apiKey: '04c45167-d015-469f-9340-ca84e1d8655c',
+          endpoint: 'https://octo.ventrata.com',
+          accountId: '2635034e-3094-428b-b8f0-9d0cc0960c0c',
+          name: 'testConnection',
+        });
+      });
 
-    it('should succeed at second retry with successful response', async () => {
-      global.fetch = vi
-        .fn()
-        .mockReturnValueOnce(Promise.resolve(new Response('503 Service Unavailable', { status: 503 })))
-        .mockReturnValueOnce(Promise.resolve(new Response('{}', { status: 200 })));
-      const response = await fetchRetry('https://octo.ventrata.com');
-      expect(response.status).toBe(200);
-    });
+      it('should succeed at first try with successful response', async () => {
+        global.fetch = vi.fn().mockReturnValueOnce(Promise.resolve(new Response('', { status: 200 })));
+        await fetchRetry('https://octo.ventrata.com', undefined, { ctx: requestContext });
 
-    it('should succeed at third retry with successful response', async () => {
-      global.fetch = vi
-        .fn()
-        .mockReturnValueOnce(Promise.resolve(new Response('503 Service Unavailable', { status: 503 })))
-        .mockReturnValueOnce(Promise.resolve(new Response('502 Bad Gateway', { status: 502 })))
-        .mockReturnValue(Promise.resolve(new Response('{}', { status: 200 })));
-      const response = await fetchRetry('https://octo.ventrata.com');
-      expect(response.status).toBe(200);
-    });
+        expect(requestContext.getSubrequest()[0].response.status).toBe(200);
+        expect(requestContext.getSubrequest().length).toBe(1);
+      });
 
-    it('should fail after three retries with client error response', async () => {
-      global.fetch = vi
-        .fn()
-        .mockReturnValueOnce(Promise.resolve(new Response('403 Forbidden', { status: 403 })))
-        .mockReturnValueOnce(Promise.resolve(new Response('401 Unauthorized', { status: 401 })))
-        .mockReturnValue(Promise.resolve(new Response('400 Bad Request', { status: 400 })));
-      const response = await fetchRetry('https://octo.ventrata.com');
-      expect(response.status).toBe(400);
-    });
+      it('should succeed at second retry with successful response', async () => {
+        global.fetch = vi
+          .fn()
+          .mockReturnValueOnce(Promise.resolve(new Response('503 Service Unavailable', { status: 503 })))
+          .mockReturnValueOnce(Promise.resolve(new Response('{}', { status: 200 })));
+        await fetchRetry('https://octo.ventrata.com', undefined, { ctx: requestContext });
 
-    it('should fail after three retries with server error response', async () => {
-      global.fetch = vi
-        .fn()
-        .mockReturnValueOnce(Promise.resolve(new Response('503 Service Unavailable', { status: 503 })))
-        .mockReturnValueOnce(Promise.resolve(new Response('502 Bad Gateway', { status: 502 })))
-        .mockReturnValue(Promise.resolve(new Response('500 Internal Server Error', { status: 500 })));
-      const response = await fetchRetry('https://octo.ventrata.com');
-      expect(response.status).toBe(500);
+        expect(requestContext.getSubrequest()[0].response.status).toBe(503);
+        expect(requestContext.getSubrequest()[1].response.status).toBe(200);
+        expect(requestContext.getSubrequest().length).toBe(2);
+      });
+
+      it('should succeed at third retry with successful response', async () => {
+        global.fetch = vi
+          .fn()
+          .mockReturnValueOnce(Promise.resolve(new Response('503 Service Unavailable', { status: 503 })))
+          .mockReturnValueOnce(Promise.resolve(new Response('502 Bad Gateway', { status: 502 })))
+          .mockReturnValueOnce(Promise.resolve(new Response('{}', { status: 200 })));
+        await fetchRetry('https://octo.ventrata.com', undefined, { ctx: requestContext });
+
+        expect(requestContext.getSubrequest()[0].response.status).toBe(503);
+        expect(requestContext.getSubrequest()[1].response.status).toBe(502);
+        expect(requestContext.getSubrequest()[2].response.status).toBe(200);
+        expect(requestContext.getSubrequest().length).toBe(3);
+      });
+
+      it('should fail after three retries with server error response', async () => {
+        global.fetch = vi
+          .fn()
+          .mockReturnValueOnce(Promise.resolve(new Response('503 Service Unavailable', { status: 503 })))
+          .mockReturnValueOnce(Promise.resolve(new Response('502 Bad Gateway', { status: 502 })))
+          .mockReturnValueOnce(Promise.resolve(new Response('500 Internal Server Error', { status: 500 })));
+        await fetchRetry('https://octo.ventrata.com', undefined, { ctx: requestContext });
+
+        const subrequests = requestContext.getSubrequest();
+        expect(subrequests[0].response.status).toBe(503);
+        expect(subrequests[1].response.status).toBe(502);
+        expect(subrequests[2].response.status).toBe(500);
+        expect(requestContext.getSubrequest().length).toBe(3);
+      });
     });
   });
 });
--- src/util/fetchRetry.ts
+++ src/util/fetchRetry.ts
@@ -1,34 +1,71 @@
+import { SubRequestContext } from '../models/SubRequestContext';
+import { BackendParams } from '../types/Backend';
+
+const DEFAULT_MAX_RETRY_ATTEMPTS = 3;
+const RETRY_DELAY_MULTIPLIER_IN_MS = 1000;
+
 export async function fetchRetry(
   input: RequestInfo,
   init?: RequestInit,
+  params: BackendParams | null = null,
   currentRetryAttempt = 0,
-  maxRetryAttempts = 3,
+  maxRetryAttempts = DEFAULT_MAX_RETRY_ATTEMPTS,
 ): Promise<Response> {
+  const subRequestContext = new SubRequestContext();
+
+  if (params !== null) {
+    const request: Request = input instanceof Request ? input : new Request(input, init);
+
+    // Init subrequest data here, so timeout counts towards the request duration
+    subRequestContext.initRequestData({
+      request,
+      requestId: params.ctx.getRequestId(),
+      accountId: params.ctx.getAccountId(),
+    });
+  }
+
   if (currentRetryAttempt > 0) {
-    await new Promise((resolve) => setTimeout(resolve, (currentRetryAttempt + 1) * 1000));
+    await new Promise((resolve) => setTimeout(resolve, (currentRetryAttempt + 1) * RETRY_DELAY_MULTIPLIER_IN_MS));
   }
 
   currentRetryAttempt++;
 
+  let res: Response | undefined;
+  let error: Error | undefined;
+
   try {
-    const res = await fetch(input, init);
+    res = await fetch(input, init);
+  } catch (e: unknown) {
+    res = new Response(null, { status: 500, statusText: 'Cant get any response data, something went horrinly wrong.' });
 
-    if (res === undefined) {
-      return await fetchRetry(input, init, currentRetryAttempt, maxRetryAttempts);
+    if (e instanceof Error) {
+      error = e;
     }
+  }
 
-    const status = res.status;
+  const requestData = subRequestContext.getRequestData(res, error);
 
-    if ((status < 200 || status >= 400) && currentRetryAttempt < maxRetryAttempts) {
-      return await fetchRetry(input, init, currentRetryAttempt, maxRetryAttempts);
-    }
+  if (currentRetryAttempt > 0) {
+    requestData.isRetry = true;
+  }
 
-    return res;
-  } catch (error: unknown) {
-    if (currentRetryAttempt < maxRetryAttempts) {
-      return await fetchRetry(input, init, currentRetryAttempt, maxRetryAttempts);
-    }
+  if (params !== null) {
+    params.ctx.addSubrequest(requestData);
+  }
 
+  if (res === undefined && currentRetryAttempt < maxRetryAttempts) {
+    return await fetchRetry(input, init, params, currentRetryAttempt, maxRetryAttempts);
+  }
+
+  const status = res.status;
+
+  if ((status < 200 || status >= 400) && currentRetryAttempt < maxRetryAttempts) {
+    return await fetchRetry(input, init, params, currentRetryAttempt, maxRetryAttempts);
+  }
+
+  if (error !== undefined) {
     throw error;
   }
+
+  return res;
 }
