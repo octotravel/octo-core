4e4d22e14ad34e29a9221f8835d6a1c8e13e05b1
Faboslav <fabianek.ales@gmail.com>
2024-09-19 11:29:47 +0200

    Added SchemaValidator

---
+++ src/util/SchemaValidator.ts
@@ -0,0 +1,40 @@
+import { AnySchema, ValidationError } from 'yup';
+import { BAD_REQUEST } from '../models/BackendError';
+import { HttpBadRequest } from '../models/Error';
+import { RequestContext } from '../models/RequestContext';
+import { AlertData } from '../models/AlertData';
+import { AlertType } from '../types/AlertType';
+
+export class SchemaValidator {
+  public static validateSchema = async <T>(
+    schema: AnySchema<T>,
+    data: unknown,
+    requestContext: RequestContext,
+    onError?: (err: Error) => T | never,
+  ): Promise<T> => {
+    try {
+      await schema.validate(data);
+      return schema.cast(data) as T;
+    } catch (err: any) {
+      if (err instanceof ValidationError || err.name === 'ValidationError') {
+        const validationErrorMessage = err.errors[0];
+
+        requestContext.enableAlert(new AlertData(AlertType.VALIDATION_ERROR, validationErrorMessage));
+
+        if (onError) {
+          const result = onError(err);
+          if (result !== undefined) {
+            return result as T;
+          }
+        } else {
+          throw new HttpBadRequest({
+            error: BAD_REQUEST,
+            errorMessage: validationErrorMessage,
+          });
+        }
+      }
+
+      throw err;
+    }
+  };
+}
