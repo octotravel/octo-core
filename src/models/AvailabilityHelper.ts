3f3d2df4b20d6952e35697f25b21c8e6fad076d9
Dominik Chrastek <dominik.chrastek@gmail.com>
2024-11-22 16:33:18 +0100

    format

--- src/models/AvailabilityHelper.ts
+++ src/models/AvailabilityHelper.ts
@@ -4,12 +4,13 @@ import { DateHelper } from './DateHelper';
 
 export class AvailabilityHelper {
   public static checkAvailability = (availabilities: Availability[], dateString: string): Availability => {
-    const availability = availabilities.find((availability) => {
-      if (availability.allDay) {
-        return DateHelper.getDate(availability.localDateTimeStart) === DateHelper.getDate(dateString)
-      }
-      return availability.localDateTimeStart === dateString
-    }) ?? null;
+    const availability =
+      availabilities.find((availability) => {
+        if (availability.allDay) {
+          return DateHelper.getDate(availability.localDateTimeStart) === DateHelper.getDate(dateString);
+        }
+        return availability.localDateTimeStart === dateString;
+      }) ?? null;
     if (availability === null) {
       throw new NoAvailabilityError();
     }
