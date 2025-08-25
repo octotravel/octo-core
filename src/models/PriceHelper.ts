748b9e23d6f4471b25764dbd0334cb5e559ef3f2
Dominik Chrastek <dominik.chrastek@gmail.com>
2025-08-25 14:52:54 +0200

    fix price helper

--- src/models/PriceHelper.ts
+++ src/models/PriceHelper.ts
@@ -3,19 +3,23 @@ import { Pricing } from '@octocloud/types';
 export interface Price {
   original: number;
   retail: number;
+  net: number | null;
 }
 
 export abstract class PriceHelper {
   public static calculatePrice = (pricing: Pricing): Price => {
     if (pricing.offerDiscount) {
       return {
-        original: pricing.original + pricing.offerDiscount.original,
+        original: pricing.original,
+        net:
+          pricing.net !== null && pricing.offerDiscount.net !== null ? pricing.net + pricing.offerDiscount.net : null,
         retail: pricing.retail + pricing.offerDiscount.retail,
       };
     }
 
     return {
       original: pricing.original,
+      net: pricing.net,
       retail: pricing.retail,
     };
   };
--- src/models/__tests__/PriceHelper.test.ts
+++ src/models/__tests__/PriceHelper.test.ts
@@ -1,4 +1,4 @@
-import { PricingModelGenerator, PricingParser } from '@octocloud/generators';
+import { PartialPricing, PricingModelGenerator, PricingParser } from '@octocloud/generators';
 import { CapabilityId, Currency } from '@octocloud/types';
 import { describe, expect, it } from 'vitest';
 import { PriceHelper } from '../PriceHelper';
@@ -6,7 +6,7 @@ import { PriceHelper } from '../PriceHelper';
 describe('PricingHelper', () => {
   const pricingModelGenerator = new PricingModelGenerator();
   const pricingParser = new PricingParser();
-  const pricingData = {
+  const pricingData: PartialPricing = {
     original: 1000,
     retail: 1000,
     net: 1000,
@@ -14,32 +14,67 @@ describe('PricingHelper', () => {
     currency: Currency.EUR,
     currencyPrecision: 2,
     offerDiscount: {
-      original: 500,
+      net: 500,
       retail: 500,
       includedTaxes: [],
+    },
+  };
+  const pricingDataNetNull: PartialPricing = {
+    ...pricingData,
+    net: null,
+    offerDiscount: {
+      ...pricingData.offerDiscount,
+      net: null,
     },
   };
   const pricingModel = pricingModelGenerator.generatePricing({
     pricingData,
     capabilities: [],
   });
+  const pricingNetNullModel = pricingModelGenerator.generatePricing({
+    pricingDataNetNull,
+    capabilities: [],
+  });
   const pricingModelWithOffers = pricingModelGenerator.generatePricing({
     pricingData,
     capabilities: [CapabilityId.Offers],
   });
+  const pricingNetNulModelWithOffers = pricingModelGenerator.generatePricing({
+    pricingDataNetNull,
+    capabilities: [CapabilityId.Offers],
+  });
+
   const pricing = pricingParser.parseModelToPOJO(pricingModel);
+  const pricingNetNull = pricingParser.parseModelToPOJO(pricingNetNullModel);
   const pricingWithOffer = pricingParser.parseModelToPOJO(pricingModelWithOffers);
+  const pricingNetNullWithOffer = pricingParser.parseModelToPOJO(pricingNetNullModel);
 
   describe('calculatePrice', () => {
     it('should calculate price', async () => {
       const price = PriceHelper.calculatePrice(pricing);
       expect(price.original).toBe(1000);
+      expect(price.net).toBe(1000);
+      expect(price.retail).toBe(1000);
+    });
+
+    it('should calculate price when net is null', async () => {
+      const price = PriceHelper.calculatePrice(pricing);
+      expect(price.original).toBe(1000);
+      expect(price.net).toBe(null);
       expect(price.retail).toBe(1000);
     });
 
     it('should calculate price with offer', async () => {
       const price = PriceHelper.calculatePrice(pricingWithOffer);
       expect(price.original).toBe(1500);
+      expect(price.net).toBe(1000);
+      expect(price.retail).toBe(1500);
+    });
+
+    it('should calculate price with offer when net is null', async () => {
+      const price = PriceHelper.calculatePrice(pricingWithOffer);
+      expect(price.original).toBe(1500);
+      expect(price.net).toBe(null);
       expect(price.retail).toBe(1500);
     });
   });
