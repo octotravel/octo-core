49298ce7deff34b38b09b905e6076a274e029779
Michal Panský <pansky.michal@gmail.com>
2025-09-18 12:35:48 +0200

    Add alertWebhookURL to BaseConfig constructor

--- src/models/Config.ts
+++ src/models/Config.ts
@@ -13,15 +13,18 @@ export class BaseConfig {
   public isStaging: boolean;
   public productionURL: string;
   public stagingURL: string;
+  public alertWebhookURL: string;
 
   public constructor({
     environment,
     productionURL,
     stagingURL,
+    alertWebhookURL,
   }: {
     environment: Environment;
     productionURL: string;
     stagingURL: string;
+    alertWebhookURL: string;
   }) {
     this.environment = environment;
     this.isProduction = environment === Environment.PRODUCTION;
@@ -30,6 +33,7 @@ export class BaseConfig {
     this.isStaging = environment === Environment.STAGING;
     this.productionURL = productionURL;
     this.stagingURL = stagingURL;
+    this.alertWebhookURL = alertWebhookURL;
   }
 
   public getURL = (): string => {
